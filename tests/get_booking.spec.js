12d0a2b27b27ba79b8043e918e730c588c25ab0f
Akash K <akash.4balhara@gmail.com>
2023-04-25 21:57:02 +0530

    added script to get booking from booking id

---
+++ tests/get_booking.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test(" Get All Booking id ", async ({ request, baseURL }) => {
+
+  // setting a random id number
+    const id = 100;
+    const response = await request.get(`${baseURL}booking/`+id, { });
+
+    // saving api response as json in object
+    const bookingDetails = await response.json();
+    // extracting first name as an example
+    console.log(bookingDetails.firstname);
+
+    // assert that the api is working as expected
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+
+    
+  });
